0440a0720103da2ec39b5266db015d78701b69f4
筍伯♪ <87149267+jack4215@users.noreply.github.com>
2021-12-10 23:15:54 +0800

    Update block.js

--- block.js
+++ block.js
@@ -3,7 +3,6 @@ liff.init({liffId: "1656185375-gBk83qL0" }).then(function (e) {
       if (isLoggedIn) {     
         liff.getProfile().then(function(data) {
           if (data.displayName == '%') {     
-            liff.logout(); 
             alert('恭喜你找出答案！');
           } 
         })
