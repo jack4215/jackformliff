0aac9668dd7a86fa7f44bf278cf41b39a1ef7d10
筍伯♪ <87149267+jack4215@users.noreply.github.com>
2021-12-10 23:11:28 +0800

    Update block.js

--- block.js
+++ block.js
@@ -5,7 +5,7 @@ liff.init({liffId: "1656185375-gBk83qL0" }).then(function (e) {
           if (data.userId == 'Ud8d9cd2290d100db0ba3eb79a6c22dbf') {     
             liff.logout(); 
             alert('發生錯誤');
-            window.location.reload();
+            liff.closeWindow()
           } 
         })
       }
