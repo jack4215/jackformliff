746088958d3f67172ced4f92ebdafc25bc538d39
筍伯♪ <87149267+jack4215@users.noreply.github.com>
2021-12-10 23:10:30 +0800

    Create block.js

---
+++ block.js
@@ -0,0 +1,13 @@
+liff.init({liffId: "1656185375-gBk83qL0" }).then(function (e) {
+      var isLoggedIn = liff.isLoggedIn();
+      if (isLoggedIn) {     
+        liff.getProfile().then(function(data) {
+          if (data.userId == 'Ud8d9cd2290d100db0ba3eb79a6c22dbf') {     
+            liff.logout(); 
+            alert('發生錯誤');
+            window.location.reload();
+          } 
+        })
+      }
+   })
+      
